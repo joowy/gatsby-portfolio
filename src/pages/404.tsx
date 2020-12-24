a11ab2b46545bdd97f9972adf6d0a381b27482b4
joowy <46057052+DooterMcDooter@users.noreply.github.com>
2020-12-24 15:50:42 -0500

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,39 +1,9 @@
 import React from "react";
-import { PageProps } from "gatsby";
-import { useI18next } from "gatsby-plugin-react-i18next";
-import { Button } from "gatsby-theme-material-ui";
-import { makeStyles, Box, Grid, Typography } from "@material-ui/core";
-import { LocalHotel as LocalHotelIcon } from "@material-ui/icons";
-import { Layout } from "../components";
+const NotFoundPage = () => (
+  <div>
+    <h1>404: Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
+);
 
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    width: theme.spacing(20),
-    height: theme.spacing(20),
-  },
-}));
-
-const NotFound: React.FC<PageProps> = ({ path }) => {
-  const classes = useStyles();
-  const { t } = useI18next();
-  return (
-    <Layout pathname={path}>
-      <Grid container direction="column" justify="center" alignItems="center">
-        <Box textAlign="center">
-          <LocalHotelIcon className={classes.icon} />
-          <Typography variant="h4" component="h1" paragraph>
-            {t("not-found.title")}
-          </Typography>
-          <Typography variant="body1">{t("not-found.description")}</Typography>
-        </Box>
-        <Box marginTop={4} textAlign="center">
-          <Button variant="contained" to="/">
-            {t("not-found.back-to-home")}
-          </Button>
-        </Box>
-      </Grid>
-    </Layout>
-  );
-};
-
-export default NotFound;
+export default NotFoundPage;
